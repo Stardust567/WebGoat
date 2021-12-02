82dd374b1c9da519d1f68632c290f6e1185d8c48
caoxj <xujiecao@foxmail.com>
2021-12-02 23:12:38 +0800

    test

--- webgoat-server/src/main/java/org/owasp/webgoat/HSQLDBDatabaseConfig.java
+++ webgoat-server/src/main/java/org/owasp/webgoat/HSQLDBDatabaseConfig.java
@@ -35,7 +35,7 @@ public class HSQLDBDatabaseConfig {
     //     return server;
     // }
 
-    @Bean(initMethod = "start", destroyMethod = "stop")
+    @Bean
     @Primary
     public DataSource dataSource() {
         return DataSourceBuilder
